33970722670ca803206a22ecb2fa7d00d8e6bc75
yanting.wan <studyshdfdjk@gmail.com>
2025-11-22 07:48:12 +0800

    -推理代码 未完成

---
+++ bins/bisenet_inference.py
@@ -0,0 +1,68 @@
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(BASE_DIR, '..'))
+import argparse
+import torch
+import cv2
+import time
+import numpy as np
+from models.build_BiSeNet import BiSeNet
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+parser = argparse.ArgumentParser(description='Training')
+parser.add_argument('--path_checkpoint', default=r"...",  # todo: 模型保存路径
+                    help="path to your dataset")
+parser.add_argument('--path_img', default=r"...",  # todo： 预测的图片路径
+                    help="path to your dataset")
+parser.add_argument('--data_root_dir', default=r"...",  # todo：数据根目录
+                    help="path to your dataset")
+args = parser.parse_args()
+
+if __name__ == '__main__':
+
+    path_img = args.path_img
+    in_size = 512  # 224， 448 ， 336 ， 1024
+    norm_mean = (0.5, 0.5, 0.5)  # 比imagenet的mean效果好
+    norm_std = (0.5, 0.5, 0.5)
+    # step1: 数据预处理
+    # todo: 数据预处理待完成
+
+    # step2: 模型加载
+    model = BiSeNet(num_classes=1, context_path="resnet101")
+    checkpoint = torch.load(args.path_checkpoint)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    model.to(device)
+    model.eval()
+
+    # step3: 推理
+    with torch.no_grad():
+        # 因为cpu/gpu是异步的，因此用cpu对GPU计时，应当等待gpu准备好再计时
+        # 即使这个操作是等待GPU全部执行结束，CPU才可以读取时间信息
+        torch.cuda.synchronize()
+        s = time.time()
+        outputs = model(img_tensor)
+        torch.cuda.synchronize()  # 该操作是等待GPU全部执行结束，CPU才可以读取信息
+        print(f"{time.time() - s:.4f}")
+        outputs = torch.sigmoid(outputs).squeeze(1)
+        pre_label = outputs.data.cpu().numpy()[0]
+
+
+    # step4：后处理显示图像
+    background = np.zeros_like(img_bgr, dtype=np.uint8)
+    background[:] = 255
+    alpha_bgr = pre_label
+    alpha_bgr = cv2.cvtColor(alpha_bgr, cv2.COLOR_GRAY2BGR)
+    h, w, c = img_bgr.shape
+    alpha_bgr = cv2.resize(alpha_bgr, (w, h))
+    # fusion
+    result = np.uint8(img_bgr * alpha_bgr + background * (1 - alpha_bgr))
+    out_img = np.concatenate([img_bgr, result], axis=1)
+    cv2.imshow("result", out_img)
+    cv2.waitKey()
+
+
+
+
